592af9a98a44454112cfe555b2a713b8b1d3e1d0
Raju Ahmed <rajuahmedcse@gmail.com>
2018-12-14 02:13:57 +0600

    save and restore implemented

--- src/editor/index.js
+++ src/editor/index.js
@@ -1,9 +1,11 @@
+import Notifications, {notify} from 'react-notify-toast';
 import { Editor } from 'slate-react';
 import { Value } from 'slate';
 import React from 'react';
+
 import EditorToolbar, { buttonTypes } from './editorToolbar';
 
-const initialValue = Value.fromJSON({
+const emptyDocument = Value.fromJSON({
   document: {
     nodes: [
       {
@@ -27,24 +29,50 @@ const initialValue = Value.fromJSON({
 class RichEditor extends React.Component {
   constructor() {
     super();
+    const initialValue = this.getInitialValue();
+    console.log('initial value is ..', initialValue);
     this.state = {
       value: initialValue,
-    }
+    };
     this.onChange = this.onChange.bind(this);
     this.handleToolbarButtonClick = this.handleToolbarButtonClick.bind(this);
+  }
+
+  getInitialValue() {
+    const existingValue = JSON.parse(localStorage.getItem('editorContent'));
+    const initialValue = existingValue || emptyDocument;
+    return Value.fromJSON(initialValue);
   }
 
   onChange({ value }) {
     this.setState({ value });
   }
 
+  saveContent() {
+    const editorContent = JSON.stringify(this.state.value.toJSON());
+    localStorage.setItem('editorContent', editorContent);
+    notify.show('💾 saved ✔', 'success');
+  }
+
+  restoreContent() {
+    const initialValue = this.getInitialValue();
+    this.setState({ value: initialValue });
+    notify.show('restored saved content ✔', 'success');
+  }
+
   handleToolbarButtonClick(event, buttonDetails) {
-    console.log('toolbar button clicked', buttonDetails);
+    const buttonType = buttonDetails.buttonType;
+    if (buttonType === buttonTypes.save) {
+      this.saveContent();
+    } else if (buttonType === buttonTypes.cancel) {
+      this.restoreContent();
+    }
   };
 
   render() {
     return (
       <div>
+        <Notifications/>
         <EditorToolbar
           activeMarks={this.state.value.activeMarks}
           blocks={this.state.value.blocks}
